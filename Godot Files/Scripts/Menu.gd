4d1d9be298b32a5f916bab23e9fb55cba531e273
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2023-12-27 18:51:19 -0500

    Added tile map and created the first level

--- Godot Files/Scripts/Menu.gd
+++ Godot Files/Scripts/Menu.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass 
+	$VBoxContainer/ButtonStart.grab_focus()
+	$MusicMenu.play()
 	#Preload the game scene here on start up
 
 
@@ -15,7 +16,10 @@ func _process(delta):
 
 
 func _on_button_start_pressed():
+	$MusicMenu.stop()
 	$ButtonSound.play()
+	
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
 func _on_button_options_pressed():
