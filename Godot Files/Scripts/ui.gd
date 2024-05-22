e9341fe8a6e22f0205bbb9b7e4cb57c4a17392cc
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2024-05-22 16:25:21 -0400

    Added a small tutorial to the game

--- Godot Files/Scripts/ui.gd
+++ Godot Files/Scripts/ui.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+var tutorialMsg = 1  #1 represents the first msg, 2 the second, etc..
+
+
+
+	
+	
 #Update health
 func _on_tank_1_dealt_player_damage(value):
 	#print(value)
@@ -20,3 +26,18 @@ func _on_button_resume_pressed():
 func _on_button_pause_pressed():
 	$PauseLabel.visible = true
 	get_tree().paused = true
+
+
+func _on_button_tutorial_pressed():
+	if tutorialMsg == 1:
+		$Tutorial/TutorialMsg.text = "Tanks will spawn and attempt to reach the end of the path, stop them by placing towers."
+		tutorialMsg = 2
+		
+	elif tutorialMsg == 2:
+		$Tutorial/TutorialMsg.text = "Unpause the game (using the \"Resume\" button) and you can begin placing towers."
+		tutorialMsg = 3
+		
+	elif tutorialMsg == 3:
+		$Tutorial/ButtonTutorial.visible = false
+		$Tutorial/TutorialMsg.visible = false
+		
