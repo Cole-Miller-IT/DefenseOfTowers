a37497db5783054c59db00fa6ffa1e5724680b4a
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2024-06-02 17:09:18 -0400

    Create a logo screen

--- Godot Files/Scripts/Menu.gd
+++ Godot Files/Scripts/Menu.gd
@@ -12,7 +12,7 @@ func _ready():
 func _on_button_start_pressed():
 	$MusicMenu.stop()
 	$ButtonSound.play()
-	
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://Scenes/level_menu.tscn")
 
 
